876220bf96bf16772336380a21075ece5e05ddbf
Enrico Zannini <enricozannini93@gmail.com>
2024-10-14 16:09:30 +0200

    [MT-1541] VisitorStorage (#283)
    * Create VisitorIdStorage
    
    * Test VisitorStorage
    
    * Add test files to project
    
    * Rename VisitorIdStorage to VisitorStorage
    
    * Move VisitorStorage, ModuleStore and ModuleStoreProvider in the Stores directory

---
+++ Example/Tests/Persistence/Stores/VisitorStorageTests.swift
@@ -0,0 +1,90 @@
+//
+//  VisitorStorageTests.swift
+//  tealium-swift
+//
+//  Created by Enrico Zannini on 09/10/24.
+//  Copyright © 2024 Tealium, Inc. All rights reserved.
+//
+
+@testable import TealiumSwift
+import XCTest
+
+final class VisitorStorageTests: XCTestCase {
+    let databaseProvider = MockDatabaseProvider()
+    func createStorage() throws -> VisitorStorage {
+        let storeProvider = ModuleStoreProvider(databaseProvider: databaseProvider,
+                                                modulesRepository: SQLModulesRepository(dbProvider: databaseProvider))
+        return try VisitorStorage(storage: storeProvider.getModuleStore(name: "visitor"))
+    }
+    var storage: VisitorStorage!
+    override func setUp() {
+        storage = XCTAssertNoThrowReturn(try createStorage())
+    }
+
+    func test_visitorId_is_null_at_the_start() {
+        XCTAssertNil(storage.visitorId)
+    }
+
+    func test_visitorId_returns_visitorId_after_changeVisitor() throws {
+        try storage.changeVisitor("aVisitor")
+        XCTAssertEqual(storage.visitorId, "aVisitor")
+    }
+
+    func test_visitorId_returns_visitorId_after_changeVisitor_withIdentity() throws {
+        try storage.changeVisitor("aVisitor", withIdentity: "anIdentity")
+        XCTAssertEqual(storage.visitorId, "aVisitor")
+    }
+
+    func test_currentIdentity_is_null_at_the_start() {
+        XCTAssertNil(storage.currentIdentity)
+    }
+
+    func test_currentIdentity_is_null_after_changeVisitor() throws {
+        try storage.changeVisitor("aVisitor")
+        XCTAssertNil(storage.currentIdentity)
+    }
+
+    func test_currentIdentity_returns_identity_after_changeVisitor_withIdentity() throws {
+        try storage.changeVisitor("aVisitor", withIdentity: "anIdentity")
+        XCTAssertEqual(storage.currentIdentity, "anIdentity")
+    }
+
+    func test_changeVisitor_withIdentity_replaces_both_visitorId_and_currentIdentity() throws {
+        try storage.changeVisitor("aVisitor", withIdentity: "anIdentity")
+        XCTAssertEqual(storage.visitorId, "aVisitor")
+        XCTAssertEqual(storage.currentIdentity, "anIdentity")
+        try storage.changeVisitor("newVisitor", withIdentity: "anotherIdentity")
+        XCTAssertEqual(storage.visitorId, "newVisitor")
+        XCTAssertEqual(storage.currentIdentity, "anotherIdentity")
+    }
+
+    func test_changeVisitor_replaces_visitorId_and_associates_identity_with_current_visitorId() throws {
+        try storage.changeVisitor("aVisitor", withIdentity: "anIdentity")
+        XCTAssertEqual(storage.getKnownVisitorId(identity: "anIdentity"), "aVisitor")
+        try storage.changeVisitor("newVisitor")
+        XCTAssertEqual(storage.getKnownVisitorId(identity: "anIdentity"), "newVisitor")
+    }
+
+    func test_getKnownVisitorId_returns_previous_visitorIds() throws {
+        try storage.changeVisitor("aVisitor", withIdentity: "anIdentity")
+        try storage.changeVisitor("newVisitor", withIdentity: "anotherIdentity")
+        XCTAssertEqual(storage.getKnownVisitorId(identity: "anIdentity"), "aVisitor")
+    }
+
+    func test_getKnownVisitorId_returns_visitorId_after_changeVisitor_withIdentity() throws {
+        try storage.changeVisitor("aVisitor", withIdentity: "anIdentity")
+        XCTAssertEqual(storage.getKnownVisitorId(identity: "anIdentity"), "aVisitor")
+    }
+
+    func test_clear_removes_all_data_and_inserts_new_visitorId() throws {
+        try storage.changeVisitor("aVisitor1", withIdentity: "anIdentity1")
+        try storage.changeVisitor("aVisitor2", withIdentity: "anIdentity2")
+        try storage.changeVisitor("aVisitor3", withIdentity: "anIdentity3")
+        try storage.clear(newVisitorId: "newVisitor")
+        XCTAssertNil(storage.getKnownVisitorId(identity: "anIdentity1"))
+        XCTAssertNil(storage.getKnownVisitorId(identity: "anIdentity2"))
+        XCTAssertNil(storage.getKnownVisitorId(identity: "anIdentity3"))
+        XCTAssertNil(storage.currentIdentity)
+        XCTAssertEqual(storage.visitorId, "newVisitor")
+    }
+}
---
+++ tealium-swift/core/Internal/Persistence/Stores/VisitorStorage.swift
@@ -0,0 +1,129 @@
+//
+//  VisitorStorage.swift
+//  tealium-swift
+//
+//  Created by Enrico Zannini on 09/10/24.
+//  Copyright © 2024 Tealium, Inc. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * `VisitorStorage` provides a set of methods to store, retrieve and associate visitors to known
+ * identities.
+ *
+ * Identities provided to these methods should be consistent and should be hashed to avoid storing
+ * personal data unnecessarily.
+ */
+class VisitorStorage {
+    enum Keys {
+        static let visitorId = "visitorId"
+        static let currentIdentity = "currentIdentity"
+    }
+    private let storage: DataStore
+    init(storage: DataStore) {
+        self.storage = storage
+    }
+
+    /**
+     * Returns the currently stored visitor id if available.
+     *
+     * This getter should only return `nil` on first creation/launch.
+     */
+    var visitorId: String? {
+        storage.get(key: Keys.visitorId)
+    }
+
+    /**
+     * Returns the currently stored identity if available.
+     */
+    var currentIdentity: String? {
+        storage.get(key: Keys.currentIdentity)
+    }
+
+    /**
+     * Sets the `VisitorStorage.visitorId` to the provided `visitorId`. It does not update the `currentIdentity`.
+     *
+     * Also stores an entry associating `currentIdentity` to `visitorId` such that a previous `visitorId`
+     * can be retrieved using `getKnownVisitorId` providing the same identity.
+     *
+     * - Parameters:
+     *  - visitorId: The visitor id to store as the current visitor id.
+     */
+    func changeVisitor(_ visitorId: String) throws {
+        try storage.edit()
+            .setVisitorId(visitorId)
+            .associateVisitor(visitorId, withIdentity: currentIdentity)
+            .commit()
+    }
+
+    /**
+     * Sets the `VisitorStorage.visitorId` to the provided `visitorId` and sets the
+     * `VisitorStorage.currentIdentity` to the provided `identity`.
+     *
+     * Also stores an entry associating `identity` to `visitorId` such that a previous `visitorId`
+     * can be retrieved using `getKnownVisitorId` providing the same `identity`.
+     *
+     * - Parameters:
+     *  - visitorId: The visitor id to store as the current visitor id.
+     *  - identity: The identity to store as the current identity, and to associate with the `visitorId`.
+     */
+    func changeVisitor(_ visitorId: String, withIdentity identity: String) throws {
+        try storage.edit()
+            .setVisitorId(visitorId)
+            .setCurrentIdentity(identity)
+            .associateVisitor(visitorId, withIdentity: identity)
+            .commit()
+    }
+
+    /**
+     * Sets the `VisitorStorage.currentIdentity` to the provided `identity`.
+     *
+     * It does not create any associations between the `currentIdentity` and the `visitorId`.
+     *
+     * - Parameters:
+     *  - identity: The identity to change to.
+     */
+    func changeIdentity(identity: String) throws {
+        try storage.edit()
+            .setCurrentIdentity(identity)
+            .commit()
+    }
+
+    /**
+     * Retrieves a previously seen visitor id that is associated with the given `identity`.
+     */
+    func getKnownVisitorId(identity: String) -> String? {
+        storage.get(key: identity)
+    }
+
+    /**
+     * Clears all stored VisitorId's and identities, and replaces the [visitorId] with the [newVisitorId]
+     *
+     * - Parameters:
+     *  - newVisitorId: the replacement visitor to save after clearing.
+     */
+    func clear(newVisitorId: String) throws {
+        try storage.edit()
+            .clear()
+            .setVisitorId(newVisitorId)
+            .commit()
+    }
+}
+
+fileprivate extension DataStoreEditor {
+    func setVisitorId(_ visitorId: String) -> Self {
+        put(key: VisitorStorage.Keys.visitorId, value: visitorId, expiry: .forever)
+    }
+
+    func setCurrentIdentity(_ identity: String) -> Self {
+        put(key: VisitorStorage.Keys.currentIdentity, value: identity, expiry: .forever)
+    }
+
+    func associateVisitor(_ visitorId: String, withIdentity identity: String?) -> Self {
+        guard let identity else {
+            return self
+        }
+        return put(key: identity, value: visitorId, expiry: .forever)
+    }
+}
